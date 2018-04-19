63ccc7ed73a98331cfe959bb5e8bde8cad0a972f
slice <ryaneft@gmail.com>
2018-04-18 22:35:31 -0700

    clean

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn ping(host: &str) -> PingResult<f64> {
     let mut ping = Ping::new();
     ping.set_timeout(5.0)?;
     ping.add_host(host)?;
-    let response = ping.send()?.next().expect("ping not found");
+    let response = ping.send()?.next().expect("ping got thrown into the void");
     println!(
         ">> {} ({}), {}ms",
         response.hostname, response.address, response.latency_ms
@@ -23,17 +23,13 @@ fn main() {
     let signal = chan_signal::notify(&[Signal::INT, Signal::TERM]);
 
     let ping_clone = total_pings.clone();
-    thread::spawn(move || {
-        loop {
-            ping("8.8.8.8").expect("failed to ping, are you running with sudo?");
-            let mut data = ping_clone.lock().unwrap();
-            *data += 1;
-            thread::sleep(Duration::from_millis(1000));
-        }
+    thread::spawn(move || loop {
+        ping("8.8.8.8").expect("failed to ping, are you running with sudo?");
+        *ping_clone.lock().unwrap() += 1;
+        thread::sleep(Duration::from_millis(1000));
     });
 
-    let signal = signal.recv().unwrap();
-    println!("died: {:?}", signal);
+    signal.recv().unwrap();
     let total_pings = total_pings.clone();
     let total_pings = total_pings.lock().unwrap();
     println!("Total pings: {}", total_pings);
